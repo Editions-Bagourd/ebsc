692ad2a3188cee9fd8d194bce3f20f5f555c2034
Arthurim <arthurbdauphine@gmail.com>
2022-10-03 17:26:35 +0100

    Base contract and tests

--- src/BookSelling.sol
+++ src/BookSelling.sol
@@ -1,59 +1,123 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract BookSelling {
-    address public owner;
+import "openzepplin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzepplin-contracts/contracts/access/Ownable.sol";
+import "openzepplin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+
+contract BookSelling is Ownable{
+    using SafeERC20 for IERC20;
+
+
+    
+    //address public owner;
+    address public editionsBagourd;
     address public customer;
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    uint256 deliveryFee;
+
+    mapping(address => bool) public supportedTokens;
+    mapping(uint256 => uint256) public bookIdsToPrices;
+
+
+
+    constructor() public Ownable() {
+        //owner=msg.sender;
+    }
+
+    function setEditionsBagourd(address a) public onlyOwner {
+        editionsBagourd=a;
+        setSupportedTokens();
+        setPrice(1,1);
+        setPrice(2,1);
+        setPrice(3,1);
+        setDeliveryFee(10);
+    }
+
+    function setSupportedTokens() public onlyOwner {
+        supportedTokens[USDC]=true;
+        supportedTokens[USDT]=true;
+        supportedTokens[DAI]=true;
+    }
+
+    function setPrice(uint256 bookId, uint256 price) public onlyOwner {
+        bookIdsToPrices[bookId] = price;
+    }
+
+    function setDeliveryFee(uint256 fee) public onlyOwner {
+        deliveryFee = fee;
+    }
 
     struct Customer {
-        address customerAdress;
+        address customerAddress;
         string customerName;
     }
 
     struct Order {
-        uint8[] bookIds;
-        uint8[] prices;
-        uint8[] quantities;
+        uint256[] bookIds;
+        uint256[] prices;
+        uint256[] quantities;
         uint256 date;
         Customer customer;
     }
 
-    function getPrices(uint8[] memory bookIds)
+    function getPrices(uint256[] memory bookIds)
         public
-        pure
-        returns (uint8[] memory)
+        view
+        returns (uint256[] memory)
     {
-        uint8[] memory prices;
-        for (uint256 i = 0; i < bookIds.length; i++) {
-            prices[i] = 1;
+        uint256[] memory prices = new uint256[](bookIds.length);
+        uint256 p;
+        for (uint i = 0; i < bookIds.length; i++) {
+            p = bookIdsToPrices[bookIds[i]];
+            require(p>0, "You are trying to buy a non existing book");
+            prices[i] = p;
         }
         return prices;
     }
 
-    function getDeliveryFee() public pure returns (uint8) {
-        uint8 deliveryFee = 1;
+    function getDeliveryFee() public view returns (uint256) {
         return deliveryFee;
+    }
+
+    function getAmountToPay(
+        uint256[] memory bookIds,
+        uint256[] memory quantities
+    ) public view
+    returns(uint256) {
+        uint256[] memory prices = getPrices(bookIds);
+        uint256 amount = sumProd(prices, quantities) + getDeliveryFee();
+        return amount;
     }
 
     function BuyBooks(
         string memory customerName,
-        uint8[] memory bookIds,
-        uint8[] memory quantities
-    ) public payable {
+        uint256[] memory bookIds,
+        uint256[] memory quantities,
+        IERC20 erc20
+    ) public payable 
+    {
+        require(supportedTokens[address(erc20)], "Token not supported.");
         Customer memory c = Customer(msg.sender, customerName);
-        uint8[] memory prices = getPrices(bookIds);
+        uint256[] memory prices = getPrices(bookIds);
         uint256 date = block.timestamp;
         Order memory order = Order(bookIds, prices, quantities, date, c);
-        uint8 deliveryFee = getDeliveryFee();
-        uint8 amount = sumProd(prices, quantities) + deliveryFee;
+        uint256 amount = getAmountToPay(bookIds, quantities);
+
+        erc20.safeTransferFrom(msg.sender, editionsBagourd, amount);
     }
 
-    function sumProd(uint8[] memory array1, uint8[] memory array2)
+    
+
+    function sumProd(uint256[] memory array1, uint256[] memory array2)
         public pure
-        returns (uint8)
+        returns (uint256)
     {
         require(array1.length == array2.length);
-        uint8 result = 0;
+        uint256 result = 0;
         for (uint256 i = 0; i < array1.length; i++) {
             result += array1[i] * array2[i];
         }
--- test/BookSelling.t.sol
+++ test/BookSelling.t.sol
@@ -3,39 +3,65 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/BookSelling.sol";
+import "openzepplin-contracts/contracts/token/ERC20/IERC20.sol";
+
 
 contract BookSellingTest is Test {
-    function setUp() public {}
-
-    function testExample() public {
-        assertTrue(true);
+    BookSelling bookSelling;
+    address CUSTOMER = 0x19D675bBb76946785249A3AD8a805260e9420CB8;
+    address EB = 0xaC156478FA93293cc7B0DBD5FbE7e2e9Abe1f121;
+    address USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    
+    function setUp() public {
+        bookSelling = new BookSelling();
     }
 
-    function testSumProd() public {
-        uint8[] memory array1 = new uint8[](3);
-        array1[0] = 1;
-        array1[1] = 1;
-        array1[2] = 1;
-        uint8[] memory array2 = new uint8[](3);
-        array2[0] = 1;
-        array2[1] = 2;
-        array2[2] = 3;
-        uint8 sp = sumProd(array1,array2);
-        console.log(sp);
-        assert(sp==6);
+    
+    function testShouldBuyABook() public {
+        bookSelling.setEditionsBagourd(EB);
+        console.log("book contract",address(bookSelling));
+
+        assertTrue(bookSelling.editionsBagourd()==EB);
+        string memory customerName = "Customer1";
+        uint256[] memory bookIds = new uint256[](3);
+        bookIds[0]=1;
+        bookIds[1]=2;
+        bookIds[2]=3;
+        uint256[] memory quantities = new uint256[](3);
+        quantities[0]=1;
+        quantities[1]=1;
+        quantities[2]=1;
+
+        IERC20 usdc = IERC20(address(USDC));
+        uint256 startingBalance = usdc.balanceOf(EB);
         
+        vm.startPrank(CUSTOMER);
+        uint256 amountToPay = bookSelling.getAmountToPay(bookIds,quantities);
+        console.log("amount",amountToPay);
+        // customer approving contract
+        usdc.approve(address(bookSelling), amountToPay);
+
+        bookSelling.BuyBooks(
+            customerName,
+            bookIds,
+            quantities,
+            usdc
+        );
+        vm.stopPrank();
+        assertTrue(startingBalance +amountToPay == usdc.balanceOf(EB));
     }
 
+    function testShouldRevertWhenTryingToBuyANonExistingBook() public {
+        uint256[] memory bookIds = new uint256[](1);
+        uint256[] memory quantities = new uint256[](1);
+        // Non existing book id
+        bookIds[0]=10;
+        quantities[0] = 1;
 
-    function sumProd(uint8[] memory array1, uint8[] memory array2)
-        public pure
-        returns (uint8)
-    {
-        require(array1.length == array2.length);
-        uint8 result = 0;
-        for (uint256 i = 0; i < array1.length; i++) {
-            result = result + array1[i] * array2[i];
-        }
-        return result;
+        vm.expectRevert("You are trying to buy a non existing book");
+        uint256 amountToPay = bookSelling.getAmountToPay(bookIds,quantities);
+        console.log(amountToPay);
     }
+
 }
+
